77c4e3346c2a986ee4b2bd8947942c05976fbcb9
Darek Wieczorek <darius.via@gmail.com>
2014-12-07 20:08:16 -0500

    Created Dog class

--- Dogs/Dogs/Program.cs
+++ Dogs/Dogs/Program.cs
@@ -10,6 +10,39 @@ namespace Dogs
     {
         static void Main(string[] args)
         {
+            Dog myDog;
+            myDog = new Dog("reksio");
+            myDog.GetName();
+            myDog.MakeNoise();
+
+            // You need this so that the program doesn't close too soon.
+            Console.ReadLine();
+        }
+    }
+
+    class Dog
+    {
+        string Name;
+        public string Gender;
+        
+        int index = 2;
+        double mass = 5.1d;
+
+        bool isMale = true; bool isPuppy = false;
+
+        public Dog(string newName)
+        {
+            Name = newName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void MakeNoise()
+        {
+            Console.WriteLine("Hau hau - " + Name);
         }
     }
 }
